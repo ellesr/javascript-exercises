6515f4588fdff3ff5d03452dfb0b736be2a8aef4
ellesr <116410947+ellesr@users.noreply.github.com>
2025-02-19 13:38:07 -0800

    Modify function to not remove punctuation

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,8 +1,7 @@
 const reverseString = function(string) {
     let array = Array.from(string);
     let reversed = [...array].reverse();
-    reversed = reversed.toString();
-    return reversed.replaceAll(",","");
+    return reversed.join('');
 };
 
 // Do not edit below this line
