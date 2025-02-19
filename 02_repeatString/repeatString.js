2ba979846f98fff0169661faa82bed70133c5d24
ellesr <116410947+ellesr@users.noreply.github.com>
2025-02-19 12:33:00 -0800

    Return error for negative numbers

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,15 @@
 const repeatString = function(string, num) {
+    
+    while (num < 0) {
+        return "ERROR";
+    }
+    
     let result = "";
     for (i = 0; i < num; i++) {
         result += string;
     }
     return result;
+
 };
 
 // Do not edit below this line
