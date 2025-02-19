ab9d2798c9302b09152cf090bb2bbf3451bf1a8d
ellesr <116410947+ellesr@users.noreply.github.com>
2025-02-19 13:34:28 -0800

    Write function to reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = Array.from(string);
+    let reversed = [...array].reverse();
+    reversed = reversed.toString();
+    return reversed.replaceAll(",","");
 };
 
 // Do not edit below this line
