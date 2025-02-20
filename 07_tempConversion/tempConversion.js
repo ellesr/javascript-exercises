76dd7cc6fe1787dc5fb2fc517eb578854999874b
ellesr <116410947+ellesr@users.noreply.github.com>
2025-02-19 16:59:52 -0800

    Write and test functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  fTemp = (fTemp - 32) / 1.8;
+  let rounded = Math.round(fTemp * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  cTemp = cTemp * 1.8 + 32;
+  let rounded = Math.round(cTemp * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
